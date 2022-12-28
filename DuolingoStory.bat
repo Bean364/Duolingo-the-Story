bf49d5970f4c218e7753dc3016b4b6a390b60dd7
tjblocks2912 <82232686+tjblocks2912@users.noreply.github.com>
2022-12-28 20:40:32 +0000

    Add files via upload

---
+++ DuolingoStory.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Warning, if you opened this game expecting the duolingo app, this isn't the app. If it is disguised as duolingo then you got trolled. Play this at your own risk! Credit to surreal entertainment and Luke Correia for the inspriation.
+pause
+echo THIS IS YOUR FINAL WARNING. IF YOU WANT TO EXIT, EXIT THIS APP NOW.
+pause
+start DuolingoPayload.vbs
+
+
